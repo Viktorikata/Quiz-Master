22929430ba10a70aac5e9cdcc193234e14eba211
Viktoria <va.2816275@yandex.com>
2025-07-01 11:45:38 +0300

    Добавлены вопросы в questions.js

---
+++ src/question.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+        question: "Какой язык используется для стилизации веб-страниц",
+        options: ["HTML", "CSS", "Kotlin", "JS"],
+        correctAnswer: 1,
+    },
+
+    {
+        question: "Что означает  в веб-разбработке",
+        options: ['Document Object Model', 'Display Object Management', 'Digital Ordinance Model', 'Desktop Oriented Mode'],
+        correctAnswer: 0,
+    },
+
+    {
+        question: "Какой тег используется для вставки изображения?",
+        options: ["<image>", "<src>", "<pic>", "<img>"],
+        correctAnswer: 3,
+    },
+];
+
+export default questions;

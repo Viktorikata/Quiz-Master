2964827e14e95687e827502233c7156da9de714c
Viktoria <va.2816275@yandex.com>
2025-07-04 13:57:03 +0300

    Добавление вопросов в Викторину

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 function Result ({score, onRestart}) {
+
+    function getWordForm(score) {
+        if (score === 1) return "балл";
+        if (score >= 2 && score <= 4) return 'балла';
+        return 'баллов';
+    }
+
     return (
         <div className="result-container">
-            <h2 className="result-text">Вы набрали {score} баллов!</h2>
+            <h2 className="result-text">
+                Вы набрали {score} {getWordForm(score)}!
+            </h2>
             <button className="restart-button" onClick={onRestart}>Пройти заново</button>
         </div>
     );
--- src/questions.js
+++ src/questions.js
@@ -16,6 +16,58 @@ const questions = [
         options: ["<image>", "<src>", "<pic>", "<img>"],
         correctAnswer: 3,
     },
+
+    {
+         question: 'Какой элемент используется для создания ссылок в HTML?',
+        options: ['<link>', '<a>', '<href>', '<url>'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Какой хук в React используется для хранения состояния?',
+        options: ['useRef', 'useEffect', 'useState', 'useMemo'],
+        correctAnswer: 2
+    },
+    {
+        question: 'Какое расширение у файлов JavaScript?',
+        options: ['.java', '.js', '.jsx', '.script'],
+        correctAnswer: 1
+    },
+    {
+        question: 'Что делает метод `map()` в JavaScript?',
+        options: [
+            'Фильтрует массив',
+            'Изменяет массив',
+            'Создаёт новый массив',
+            'Удаляет элементы'
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: 'Какой тег используется для создания заголовка?',
+        options: ['<heading>', '<h>', '<h1>', '<title>'],
+        correctAnswer: 2
+    },
+    {
+        question: 'Что означает CSS?',
+        options: [
+            'Creative Style Sheets',
+            'Cascading Style Sheets',
+            'Computer Style Syntax',
+            'Colorful Styling Structure'
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: 'Какой способ центрует элемент по горизонтали в Flexbox?',
+        options: [
+            'align-items: center',
+            'flex-direction: center',
+            'justify-content: center',
+            'text-align: center'
+         ],
+        correctAnswer: 2
+    }
+
 ];
 
 export default questions;
